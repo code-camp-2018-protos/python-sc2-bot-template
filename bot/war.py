4dc5a116192f16d043f4a5db013c893f16f5d724
Jesse Luoto <jesse.luoto@gmail.com>
2018-06-08 10:33:27 +0300

    Add the worst fighting bot ever

--- bot/war.py
+++ bot/war.py
@@ -1,6 +1,17 @@
+import sc2
+from sc2.constants import *
+
+
 class War():
     def __init__(self, api):
         self.api = api
+        self.first_worker_tag = None
 
     async def on_step(self, iteration):
-        await self.chat_send("ATTACK!!!")
+        #first_nexus = self.api.units(NEXUS).ready.first
+        if self.api.workers.find_by_tag(self.first_worker_tag) is None:
+            self.first_worker_tag = self.api.workers.first.tag
+
+        first_worker = self.api.workers.find_by_tag(self.first_worker_tag)
+
+        await self.api.do(first_worker.attack(self.api.enemy_start_locations[0]))
