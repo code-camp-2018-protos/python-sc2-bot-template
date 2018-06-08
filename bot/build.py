de36bdea0b010c705a061d3663d32216875a80ec
Jesse Luoto <jesse.luoto@gmail.com>
2018-06-08 10:09:23 +0300

    Add build & war classes

---
+++ bot/build.py
@@ -0,0 +1,6 @@
+class Build():
+    def __init__(self, api):
+        self.api = api
+
+    async def on_step(self, iteration):
+        await self.chat_send("BUILD!!!")
--- bot/main.py
+++ bot/main.py
@@ -2,11 +2,18 @@ import json
 from pathlib import Path
 
 import sc2
+from .war import War
+from .build import Build
+
 
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
 
+    def __init__(self):
+        self.war = War(self)
+        self.build = Build(self)
+
     async def on_step(self, iteration):
-        if iteration == 0:
-            await self.chat_send(f"Name: {self.NAME}")
+        self.war.on_step(iteration)
+        self.build.on_step(iteration)
