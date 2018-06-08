ad7f1653c6320bfdf626dc509c26f5e65019ed65
Jesse Luoto <jesse.luoto@gmail.com>
2018-06-08 13:59:55 +0300

    Add cybernetics core upgrades

--- bot/upgrader.py
+++ bot/upgrader.py
@@ -9,16 +9,22 @@ class Upgrader():
     async def on_step(self, iteration):
         for unit, upgrades in UPGRADE_TACTICS.items():
             if self.api.units(unit).empty or len(upgrades) == 0:
-                return
+                continue
 
             first_unit = self.api.units(unit).first
+            if not first_unit.is_ready:
+                continue
+
             first_upgrade = upgrades[0]
 
             if self.api.can_afford(first_upgrade):
                 await self.api.do(first_unit(first_upgrade))
                 upgrades.remove(first_upgrade)
+                return
 
 
 UPGRADE_TACTICS = {
-    FORGE: [FORGERESEARCH_PROTOSSGROUNDWEAPONSLEVEL1, FORGERESEARCH_PROTOSSGROUNDARMORLEVEL1, FORGERESEARCH_PROTOSSSHIELDSLEVEL1]
+    FORGE: [FORGERESEARCH_PROTOSSGROUNDWEAPONSLEVEL1, FORGERESEARCH_PROTOSSGROUNDARMORLEVEL1, FORGERESEARCH_PROTOSSSHIELDSLEVEL1],
+    CYBERNETICSCORE: [CYBERNETICSCORERESEARCH_PROTOSSAIRWEAPONSLEVEL1, CYBERNETICSCORERESEARCH_PROTOSSAIRARMORLEVEL1, CYBERNETICSCORERESEARCH_RESEARCHHALLUCINATION],
+    TWILIGHTCOUNCIL: []
 }
